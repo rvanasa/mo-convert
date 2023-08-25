7489d2235b1d99da5226a6f687fcaaed67eb4195
rvanasa <ryan.vandersmith@dfinity.org>
2023-08-25 15:57:30 -0600

    Consolidate regex logic

--- generate/index.js
+++ generate/index.js
@@ -7,16 +7,14 @@ fs.readdirSync(baseDocsDir).forEach((file) => {
   if (file.endsWith('.md')) {
     const content = fs.readFileSync(path.join(baseDocsDir, file), 'utf8');
     const functions = [];
-    const functionRegex = /##\s*Function\s*`([^`]+)`\s*\n```([\s\S]*?)```/g;
+    const functionRegex =
+      /##\s*Function\s*`([^`]+)`\s*\n```(?:\s*motoko (?:no-repl)?\s*\n)([\s\S]*?)```/g;
     let match;
     while ((match = functionRegex.exec(content)) !== null) {
       const functionName = match[1].trim();
       let functionDefinition = match[2].trim();
 
-      functionDefinition = functionDefinition.replace(
-        /^motoko no-repl\s*/i,
-        '',
-      );
+      functionDefinition = functionDefinition.replace(/^\s*/i, '');
       functions.push({ name: functionName, definition: functionDefinition });
     }
 
